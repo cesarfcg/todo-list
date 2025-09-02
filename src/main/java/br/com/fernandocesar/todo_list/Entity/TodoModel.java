c2e91cc9b39cec981bcb47e432a460b1503f842c
Fernando César <fernandocesar4141@gmail.com>
2025-09-02 15:51:20 -0300

    Organizando os pacotes e criando a entidade 'Todos'

---
+++ src/main/java/br/com/fernandocesar/todo_list/Entity/TodoModel.java
@@ -0,0 +1,61 @@
+package br.com.fernandocesar.todo_list.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Todos")
+public class TodoModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String descricao;
+    private boolean realizado ;
+    private int prioridade;
+
+    public TodoModel() {
+    }
+
+    public TodoModel(String nome, String descricao, boolean realizado, int prioridade) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.realizado = realizado;
+        this.prioridade = prioridade;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isRealizado() {
+        return realizado;
+    }
+
+    public void setRealizado(boolean realizado) {
+        this.realizado = realizado;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+}
