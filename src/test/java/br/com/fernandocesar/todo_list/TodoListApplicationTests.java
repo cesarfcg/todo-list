f29ca24a24810813d8fc8fe1e13490f3361380d7
Fernando César <fernandocesar4141@gmail.com>
2025-09-30 22:53:39 -0300

    Implementando o Test com webflux (Erro na validação de Array 'isArray')

--- src/test/java/br/com/fernandocesar/todo_list/TodoListApplicationTests.java
+++ src/test/java/br/com/fernandocesar/todo_list/TodoListApplicationTests.java
@@ -1,13 +1,28 @@
 package br.com.fernandocesar.todo_list;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import br.com.fernandocesar.todo_list.Entity.TodoModel;
 
 @SpringBootTest
 class TodoListApplicationTests {
-
+	@Autowired
+	private WebTestClient webTestClient;
 	@Test
 	void contextLoads() {
+		var todo = new TodoModel(null, null, false, 0);
+		webTestClient
+			.post()
+			.uri("/todos")
+			.bodyValue(todo)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody()
+			.json("$").isArray()
+			
 	}
 
 }
